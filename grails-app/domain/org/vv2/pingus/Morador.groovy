dc5cc7a19ae4846b10f17acf91d7b2db55de42dc
Pedro Rogoski <pedro.mrogoski@gmail.com>
2021-05-22 14:45:01 -0300

    Continuacao

--- grails-app/domain/org/vv2/pingus/Morador.groovy
+++ grails-app/domain/org/vv2/pingus/Morador.groovy
@@ -4,14 +4,12 @@ class Morador {
 
     String  nome
     String  rg
-    String  cpf
     Integer apto
     boolean inativo
 
     static constraints = {
         nome    nullable: false, matches: /[A-Za-z _]*[A-Za-z][A-Za-z _]*/
         rg      nullable: false, matches: /\d{2}.\d{3}.\d{3}-\d{1}|\d{9}/
-        cpf     nullable: false, matches: /\d{3}.\d{3}.\d{3}-\d{2}|\d{11}/
         apto    nullable: false, min: 0
         inativo()
     }
