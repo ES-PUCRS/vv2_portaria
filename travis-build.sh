8ca043d8dbfb181a7caa12f5605c322632b08421
HCley <Cleysonb.oliveira@gmail.com>
2021-05-25 20:56:24 -0300

    Generating log on travis build

--- travis-build.sh
+++ travis-build.sh
@@ -3,6 +3,6 @@
 EXIT_STATUS=0
 
 echo "Running tests with geb.env chromeHeadless"
-./gradlew -Dgeb.env=chromeHeadless check || EXIT_STATUS=$? #
+./gradlew -Dgeb.env=dev check || EXIT_STATUS=$? > Travis-Build_Log.txt
 
 exit $EXIT_STATUS
