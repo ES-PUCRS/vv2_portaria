bfed13852d46492868395a3f187c45474f956512
HCley <Cleysonb.oliveira@gmail.com>
2021-05-25 23:42:46 -0300

    Adicionado testes pelo framework

--- travis-build.sh
+++ travis-build.sh
@@ -6,10 +6,20 @@ EXIT_STATUS=0
 
 chmod 777 ./gradlew
 
-echo "Running tests with geb.env chromeHeadless"
+echo "Running tests with gradle geb.env chromeHeadless"
 ./gradlew -Dgeb.env=chromeHeadless check || EXIT_STATUS=$?
 
-echo "Running tests with geb.env firefoxHeadless"
+echo "Running tests with gradle geb.env firefoxHeadless"
 ./gradlew -Dgeb.env=firefoxHeadless iT || EXIT_STATUS=$?
+
+
+
+chmod 777 ./grailsw
+
+echo "Running tests with grailsw unit"
+./grailsw test-app -unit
+
+echo "Running tests with grailsw integration"
+./grailsw test-app -integration
 
 exit $EXIT_STATUS
