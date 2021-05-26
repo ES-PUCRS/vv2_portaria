401a47252eb02809108c66ce15462436f5928a79
HCley <Cleysonb.oliveira@gmail.com>
2021-05-25 22:44:53 -0300

    Permissoes ao gradlew

--- travis-build.sh
+++ travis-build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+set -e
+
 EXIT_STATUS=0
 
 echo "Running tests with geb.env chromeHeadless"
+
+chmod 777 ./gradlew
 ./gradlew -Dgeb.env=chromeHeadless check || EXIT_STATUS=$? #
 
 exit $EXIT_STATUS
