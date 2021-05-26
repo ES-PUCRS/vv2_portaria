fe1a882bb076a2c7468a5b0832af5c4759a8eef8
HCley <Cleysonb.oliveira@gmail.com>
2021-05-25 22:41:56 -0300

    Make travis-build.sh executable

--- travis-build.sh
+++ travis-build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-set -e
 
-export EXIT_STATUS=0
+EXIT_STATUS=0
 
-curl -O https://raw.githubusercontent.com/grails/grails-guides/master/travis/build-guide
-chmod 777 build-guide
-
-./build-guide || EXIT_STATUS=$?
+echo "Running tests with geb.env chromeHeadless"
+./gradlew -Dgeb.env=chromeHeadless check || EXIT_STATUS=$? #
 
 exit $EXIT_STATUS
