d65081f346cf96c70fec90b9138a0db1fde5efbf
HCley <Cleysonb.oliveira@gmail.com>
2021-05-30 21:21:30 -0300

    Removido linha inútil

--- grails-app/services/org/vv2/pingus/EntregaCustomService.groovy
+++ grails-app/services/org/vv2/pingus/EntregaCustomService.groovy
@@ -10,12 +10,11 @@ class EntregaCustomService {
     EntregaService entregaService
 
     Entrega save(Entrega entrega) {
-        if (entrega?.retirado != null) {
+        if (entrega?.retirado != null)
             throw new IllegalArgumentException("The package has already been taken")
-        } else {
-            entrega.retirado = entrega.lastUpdated
-            entregaService.save(entrega);
-        }
+
+        entrega.retirado = entrega.lastUpdated
+        entregaService.save(entrega);
     }
 
 
